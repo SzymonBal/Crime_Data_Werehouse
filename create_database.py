d86e6c5eb9c7f158d8d743b44664b24669178090
SzymonBal <szymekuj@interia.pl>
2024-04-25 09:28:09 +0200

    create database using pyodbc

---
+++ create_database.py
@@ -0,0 +1,32 @@
+import pyodbc
+
+# Parametry połączenia
+server = 'localhost'
+database = 'master'  # Połącz się z bazą master, aby móc utworzyć nową bazę danych
+username = 'sa'
+password = 'YourStrongPassword123'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+# Tworzenie ciągu połączenia
+conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+# Nawiązanie połączenia
+conn = pyodbc.connect(conn_str)
+
+# Utworzenie kursora
+cursor = conn.cursor()
+
+# Polecenie SQL CREATE DATABASE
+create_db_query = "CREATE DATABASE NowaBazaDanych2"
+
+# Wykonanie polecenia
+cursor.execute(create_db_query)
+
+# Zatwierdzenie zmian
+conn.commit()
+
+# Zamykanie kursora i połączenia
+cursor.close()
+conn.close()
+
+print("Baza danych NowaBazaDanych2 została utworzona.")
