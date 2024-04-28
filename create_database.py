d4db81e7e7aed624ce5d750d76fbce87bdd42d57
SzymonBal <szymekuj@interia.pl>
2024-04-28 14:05:18 +0200

    correct creation of dabase using autocommit

--- create_database.py
+++ create_database.py
@@ -12,12 +12,12 @@ conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};
 
 # Nawiązanie połączenia
 conn = pyodbc.connect(conn_str)
-
+conn.autocommit = True
 # Utworzenie kursora
 cursor = conn.cursor()
 
 # Polecenie SQL CREATE DATABASE
-create_db_query = "CREATE DATABASE NowaBazaDanych2"
+create_db_query = "CREATE DATABASE After_ETL"
 
 # Wykonanie polecenia
 cursor.execute(create_db_query)
@@ -29,4 +29,4 @@ conn.commit()
 cursor.close()
 conn.close()
 
-print("Baza danych NowaBazaDanych2 została utworzona.")
+print("Baza danych Clean_Database_1 została utworzona.")
