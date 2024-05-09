d2ed0467e8a0943e210df70030102e2fbb76babd
ZbiegMichal <michalocohodzi@gmail.com>
2024-05-09 12:59:45 +0200

    transform into functions

--- create_database.py
+++ create_database.py
@@ -1,4 +1,4 @@
-import pyodbc
+'''import pyodbc
 
 # Parametry połączenia
 server = 'localhost'
@@ -30,4 +30,33 @@ conn.commit()
 cursor.close()
 conn.close()
 
-print("Baza danych Clean_Database_1 została utworzona.")
+print("Baza danych Clean_Database_1 została utworzona.")'''
+
+import pyodbc
+
+def create_database(database_name, server='localhost', username='sa', password='YourStrongPassword123', driver='{ODBC Driver 17 for SQL Server}'):
+    # Tworzenie ciągu połączenia
+    conn_str = f'DRIVER={driver};SERVER={server};DATABASE=master;UID={username};PWD={password}'
+
+    try:
+        # Nawiązanie połączenia
+        conn = pyodbc.connect(conn_str)
+        conn.autocommit = True
+        # Utworzenie kursora
+        cursor = conn.cursor()
+        # Polecenie SQL CREATE DATABASE
+        create_db_query = f"CREATE DATABASE {database_name}"
+        # Wykonanie polecenia
+        cursor.execute(create_db_query)
+        # Zatwierdzenie zmian
+        conn.commit()
+        print(f"Baza danych {database_name} została utworzona.")
+    except Exception as e:
+        print(f"Wystąpił błąd podczas tworzenia bazy danych: {str(e)}")
+    finally:
+        # Zamykanie kursora i połączenia
+        cursor.close()
+        conn.close()
+
+# Przykładowe użycie funkcji
+#create_database('BAZUNIA2')
